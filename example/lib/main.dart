c18224156b6d643b4620856e62d2d59d717fc64c
Florian Weinaug <florianweinaug@outlook.de>
2020-03-29 20:25:55 +0200

    initial commit

---
+++ example/lib/main.dart
@@ -0,0 +1,49 @@
+import 'package:expandable_text/expandable_text.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Expandable Text Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Expandable Text'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            ExpandableText(
+              'Short text',
+              expandText: 'show more',
+              collapseText: 'show less',
+            ),
+            SizedBox(height: 10.0),
+            ExpandableText(
+              'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
+              expandText: 'show more',
+              collapseText: 'show less',
+              maxLines: 1,
+              linkColor: Colors.blue,
+            ),
+          ],
+        ),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
---
+++ lib/expandable_text.dart
@@ -0,0 +1,159 @@
+library expandable_text;
+
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+
+class ExpandableText extends StatefulWidget {
+  const ExpandableText(
+    this.text, {
+    Key key,
+    @required this.expandText,
+    @required this.collapseText,
+    this.expanded = false,
+    this.linkColor,
+    this.style,
+    this.textDirection,
+    this.textAlign,
+    this.textScaleFactor,
+    this.maxLines = 2,
+    this.semanticsLabel,
+  }) : assert(text != null),
+       assert(expandText != null),
+       assert(collapseText != null),
+       assert(expanded != null),
+       assert(maxLines != null && maxLines > 0),
+       super(key: key);
+
+  final String text;
+  final String expandText;
+  final String collapseText;
+  final bool expanded;
+  final Color linkColor;
+  final TextStyle style;
+  final TextDirection textDirection;
+  final TextAlign textAlign;
+  final double textScaleFactor;
+  final int maxLines;
+  final String semanticsLabel;
+
+  @override
+  ExpandableTextState createState() => ExpandableTextState();
+}
+
+class ExpandableTextState extends State<ExpandableText> {
+  bool _expanded = false;
+  TapGestureRecognizer _tapGestureRecognizer;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _expanded = widget.expanded;
+    _tapGestureRecognizer = TapGestureRecognizer()
+      ..onTap = _toggleExpanded;
+  }
+
+  @override
+  void dispose() {
+    _tapGestureRecognizer.dispose();
+    super.dispose();
+  }
+
+  void _toggleExpanded() {
+    setState(() => _expanded = !_expanded);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final DefaultTextStyle defaultTextStyle = DefaultTextStyle.of(context);
+    TextStyle effectiveTextStyle = widget.style;
+    if (widget.style == null || widget.style.inherit) {
+      effectiveTextStyle = defaultTextStyle.style.merge(widget.style);
+    }
+
+    final textAlign = widget.textAlign ?? defaultTextStyle.textAlign ?? TextAlign.start;
+    final textDirection = widget.textDirection ?? Directionality.of(context);
+    final textScaleFactor = widget.textScaleFactor ?? MediaQuery.textScaleFactorOf(context);
+    final locale = Localizations.localeOf(context, nullOk: true);
+
+    final linkText = _expanded ? ' ${widget.collapseText}' : '\u2026 ${widget.expandText}';
+    final linkColor = widget.linkColor ?? Theme.of(context).accentColor;
+
+    final link = TextSpan(
+      text: linkText,
+      style: effectiveTextStyle.copyWith(
+        color: linkColor,
+      ),
+      recognizer: _tapGestureRecognizer,
+    );
+
+    final text = TextSpan(
+      text: widget.text,
+      style: effectiveTextStyle,
+    );
+
+    Widget result = LayoutBuilder(
+      builder: (BuildContext context, BoxConstraints constraints) {
+        assert(constraints.hasBoundedWidth);
+        final double maxWidth = constraints.maxWidth;
+
+        TextPainter textPainter = TextPainter(
+          text: link,
+          textAlign: textAlign,
+          textDirection: textDirection,
+          textScaleFactor: textScaleFactor,
+          maxLines: widget.maxLines,
+          locale: locale,
+        );
+        textPainter.layout(minWidth: constraints.minWidth, maxWidth: maxWidth);
+        final linkSize = textPainter.size;
+
+        textPainter.text = text;
+        textPainter.layout(minWidth: constraints.minWidth, maxWidth: maxWidth);
+        final textSize = textPainter.size;
+
+        final position = textPainter.getPositionForOffset(Offset(
+          textSize.width - linkSize.width,
+          textSize.height,
+        ));
+        final endOffset = textPainter.getOffsetBefore(position.offset);
+
+        TextSpan textSpan;
+        if (textPainter.didExceedMaxLines) {
+          textSpan = TextSpan(
+            style: effectiveTextStyle,
+            text: _expanded
+                ? widget.text
+                : widget.text.substring(0, endOffset),
+            children: <TextSpan>[
+              link,
+            ],
+          );
+        } else {
+          textSpan = text;
+        }
+
+        return RichText(
+          text: textSpan,
+          softWrap: true,
+          textDirection: textDirection,
+          textAlign: textAlign,
+          textScaleFactor: textScaleFactor,
+          overflow: TextOverflow.clip,
+        );
+      },
+    );
+
+    if (widget.semanticsLabel != null) {
+      result = Semantics(
+        textDirection: widget.textDirection,
+        label: widget.semanticsLabel,
+        child: ExcludeSemantics(
+          child: result,
+        ),
+      );
+    }
+
+    return result;
+  }
+}
