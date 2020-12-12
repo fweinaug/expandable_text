8e6c211edc8386de0a2633165f503edda822b8a8
Florian Weinaug <florianweinaug@outlook.de>
2020-12-12 17:21:07 +0100

    add widget tests

---
+++ test/finder.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+Finder findTextSpan(bool Function(TextSpan) predicate) {
+  return find.byWidgetPredicate((widget) => _isRichTextWithTextSpan(widget, predicate));
+}
+
+Finder findTextSpanByText(String text) {
+  return find.byWidgetPredicate((widget) => _isRichTextWithTextSpan(widget, (span) => _hasText(span, text)));
+}
+
+Finder findTextSpanByTextAndColor(String text, Color color) {
+  return find.byWidgetPredicate((widget) => _isRichTextWithTextSpan(widget, (span) => _hasText(span, text) && _hasColor(span, color)));
+}
+
+bool _hasText(TextSpan span, String text) => span.toPlainText().trim() == text;
+bool _hasColor(TextSpan span, Color color) => span.style?.color == color;
+
+bool _isRichTextWithTextSpan(Widget widget, bool Function(TextSpan) predicate) {
+  bool containsTextSpan(InlineSpan span, bool Function(TextSpan) predicate) {
+    if (span is TextSpan) {
+      if (predicate(span)) {
+        return true;
+      } else if (span.children != null && span.children.length > 0) {
+        for (InlineSpan child in span.children) {
+          if (containsTextSpan(child, predicate)) {
+            return true;
+          }
+        }
+      }
+    }
+    return false;
+  }
+
+  return widget is RichText && containsTextSpan(widget.text, predicate);
+}
---
+++ test/widget_test.dart
@@ -0,0 +1,101 @@
+import 'package:expandable_text/expandable_text.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'finder.dart';
+
+const SMALL_SCREEN = Size(10, 400);
+const LARGE_SCREEN = Size(1000, 400);
+const TEXT = 'A long text to test the widget';
+
+void main() {
+  testWidgets('Collapsed widget shows truncated text with ellipsis', (WidgetTester tester) async {
+    final TestWidgetsFlutterBinding binding = TestWidgetsFlutterBinding.ensureInitialized();
+    await binding.setSurfaceSize(SMALL_SCREEN);
+
+    await tester.pumpWidget(
+      MaterialApp(home: ExpandableText(TEXT, expandText: 'more', collapseText: 'less', expanded: false)),
+    );
+
+    expect(findTextSpan((span) => span.text == TEXT), findsNothing);
+    expect(findTextSpan((span) => TEXT.startsWith(span.text)), findsOneWidget);
+    expect(findTextSpanByText('\u2026'), findsOneWidget);
+  });
+
+  testWidgets('Expanded widget shows full text', (WidgetTester tester) async {
+    final TestWidgetsFlutterBinding binding = TestWidgetsFlutterBinding.ensureInitialized();
+    await binding.setSurfaceSize(SMALL_SCREEN);
+
+    await tester.pumpWidget(
+      MaterialApp(home: ExpandableText(TEXT, expandText: 'more', collapseText: 'less', expanded: true)),
+    );
+
+    expect(findTextSpan((span) => span.text == TEXT), findsOneWidget);
+    expect(findTextSpanByText('\u2026'), findsNothing);
+  });
+
+  testWidgets('Non-expandable widget shows no link', (WidgetTester tester) async {
+    final TestWidgetsFlutterBinding binding = TestWidgetsFlutterBinding.ensureInitialized();
+    await binding.setSurfaceSize(LARGE_SCREEN);
+
+    await tester.pumpWidget(
+      MaterialApp(home: ExpandableText(TEXT, expandText: 'more', collapseText: 'less', expanded: false)),
+    );
+
+    expect(findTextSpanByText('\u2026'), findsNothing);
+    expect(findTextSpanByText('more'), findsNothing);
+    expect(findTextSpanByText('less'), findsNothing);
+  });
+
+  testWidgets('Collapsed widget shows link with expand text', (WidgetTester tester) async {
+    final TestWidgetsFlutterBinding binding = TestWidgetsFlutterBinding.ensureInitialized();
+    await binding.setSurfaceSize(SMALL_SCREEN);
+
+    await tester.pumpWidget(
+      MaterialApp(home: ExpandableText(TEXT, expandText: 'more', collapseText: 'less', expanded: false)),
+    );
+
+    expect(findTextSpanByText('\u2026'), findsOneWidget);
+    expect(findTextSpanByText('more'), findsOneWidget);
+    expect(findTextSpanByText('less'), findsNothing);
+  });
+
+  testWidgets('Expanded widget shows link with collapse text', (WidgetTester tester) async {
+    final TestWidgetsFlutterBinding binding = TestWidgetsFlutterBinding.ensureInitialized();
+    await binding.setSurfaceSize(SMALL_SCREEN);
+
+    await tester.pumpWidget(
+      MaterialApp(home: ExpandableText(TEXT, expandText: 'more', collapseText: 'less', expanded: true)),
+    );
+
+    expect(findTextSpanByText('\u2026'), findsNothing);
+    expect(findTextSpanByText('more'), findsNothing);
+    expect(findTextSpanByText('less'), findsOneWidget);
+  });
+
+  testWidgets('Ellipsis has the link color if linkEllipsis is true', (WidgetTester tester) async {
+    final TestWidgetsFlutterBinding binding = TestWidgetsFlutterBinding.ensureInitialized();
+    await binding.setSurfaceSize(SMALL_SCREEN);
+
+    await tester.pumpWidget(
+      MaterialApp(
+          home: ExpandableText(TEXT, expandText: 'more', collapseText: 'less', expanded: false, linkEllipsis: true, linkColor: Colors.red)),
+    );
+
+    expect(findTextSpanByText('\u2026'), findsOneWidget);
+    expect(findTextSpanByTextAndColor('\u2026', Colors.red), findsOneWidget);
+  });
+
+  testWidgets('Ellipsis has NOT the link color if linkEllipsis is false', (WidgetTester tester) async {
+    final TestWidgetsFlutterBinding binding = TestWidgetsFlutterBinding.ensureInitialized();
+    await binding.setSurfaceSize(SMALL_SCREEN);
+
+    await tester.pumpWidget(
+      MaterialApp(
+          home: ExpandableText(TEXT, expandText: 'more', collapseText: 'less', expanded: false, linkEllipsis: false, linkColor: Colors.red)),
+    );
+
+    expect(findTextSpanByText('\u2026'), findsOneWidget);
+    expect(findTextSpanByTextAndColor('\u2026', Colors.red), findsNothing);
+  });
+}
