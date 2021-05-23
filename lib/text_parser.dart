2a26071404d860924a66a52d1430e58dd3175ddd
Florian Weinaug <florianweinaug@outlook.de>
2021-05-23 14:09:17 +0200

    add text parser

---
+++ lib/text_parser.dart
@@ -0,0 +1,97 @@
+class TextSegment {
+  String text;
+
+  final String? name;
+  final bool isHashtag;
+  final bool isMention;
+  final bool isUrl;
+
+  bool get isText => !isHashtag && !isMention && !isUrl;
+
+  TextSegment(this.text,
+      [this.name,
+      this.isHashtag = false,
+      this.isMention = false,
+      this.isUrl = false]);
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is TextSegment &&
+          runtimeType == other.runtimeType &&
+          text == other.text &&
+          name == other.name &&
+          isHashtag == other.isHashtag &&
+          isMention == other.isMention &&
+          isUrl == other.isUrl;
+
+  @override
+  int get hashCode =>
+      text.hashCode ^
+      name.hashCode ^
+      isHashtag.hashCode ^
+      isMention.hashCode ^
+      isUrl.hashCode;
+}
+
+/// Split the string into multiple instances of [TextSegment] for mentions, hashtags, URLs and regular text.
+///
+/// Mentions are all words that start with @, e.g. @mention.
+/// Hashtags are all words that start with #, e.g. #hashtag.
+List<TextSegment> parseText(String? text) {
+  final segments = <TextSegment>[];
+
+  if (text == null || text.isEmpty) {
+    return segments;
+  }
+
+  // parse urls and words starting with @ (mention) or # (hashtag)
+  RegExp exp = RegExp(
+      r'(?<keyword>(#|@)([\p{Alphabetic}\p{Mark}\p{Decimal_Number}\p{Connector_Punctuation}\p{Join_Control}]+)|(?<url>(?:(?:https?|ftp):\/\/)?[\w/\-?=%.]+\.[\w/\-?=%.]+))',
+      unicode: true);
+  final matches = exp.allMatches(text);
+
+  var start = 0;
+  matches.forEach((match) {
+    // text before the keyword
+    if (match.start > start) {
+      if (segments.isNotEmpty && segments.last.isText) {
+        segments.last.text += text.substring(start, match.start);
+      } else {
+        segments.add(TextSegment(text.substring(start, match.start)));
+      }
+      start = match.start;
+    }
+
+    final url = match.namedGroup('url');
+    final keyword = match.namedGroup('keyword');
+
+    if (url != null) {
+      segments.add(TextSegment(url, url, false, false, true));
+    } else if (keyword != null) {
+      final isWord = match.start == 0 || text.substring(match.start - 1, start) == ' ';
+      if (!isWord) {
+        return;
+      }
+
+      final isHashtag = keyword.startsWith('#');
+      final isMention = keyword.startsWith('@');
+
+      segments.add(
+          TextSegment(keyword, keyword.substring(1), isHashtag, isMention));
+    }
+
+    start = match.end;
+  });
+
+  // text after the last keyword or the whole text if it does not contain any keywords
+  if (start < text.length) {
+    if (segments.isNotEmpty && segments.last.isText) {
+      segments.last.text += text.substring(start);
+    } else {
+      segments.add(TextSegment(text.substring(start)));
+    }
+  }
+
+  return segments;
+}
---
+++ test/parser_test.dart
@@ -0,0 +1,92 @@
+import 'package:expandable_text/text_parser.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test('Parse empty text', () {
+    final segments = parseText('');
+
+    expect(segments, []);
+  });
+
+  test('Parse simple text', () {
+    final segments = parseText('text without hashtags and mentions');
+
+    expect(segments, [
+      TextSegment('text without hashtags and mentions')
+    ]);
+  });
+
+  test('Parse hashtag', () {
+    final segments = parseText('#hashtag');
+
+    expect(segments, [
+      TextSegment('#hashtag', 'hashtag', true, false, false),
+    ]);
+  });
+
+  test('Parse text with a hashtag', () {
+    final segments = parseText('text with a #hashtag');
+
+    expect(segments, [
+      TextSegment('text with a '),
+      TextSegment('#hashtag', 'hashtag', true, false, false),
+    ]);
+  });
+
+  test('Parse mention', () {
+    final segments = parseText('@mention');
+
+    expect(segments, [
+      TextSegment('@mention', 'mention', false, true, false)
+    ]);
+  });
+
+  test('Parse text with a mention', () {
+    final segments = parseText('text with a @mention');
+
+    expect(segments, [
+      TextSegment('text with a '),
+      TextSegment('@mention', 'mention', false, true, false)
+    ]);
+  });
+
+  test('Parse url', () {
+    final segments = parseText('https://flutter.dev');
+
+    expect(segments, [
+      TextSegment('https://flutter.dev', 'https://flutter.dev', false, false, true),
+    ]);
+  });
+
+  test('Parse text with a url', () {
+    final segments = parseText('text with url https://flutter.dev');
+
+    expect(segments, [
+      TextSegment('text with url '),
+      TextSegment('https://flutter.dev', 'https://flutter.dev', false, false, true),
+    ]);
+  });
+
+  test('Parse text with mentions, hashtags and links', () {
+    final segments = parseText('text with a #hashtag and a @mention. https://flutter.dev #works');
+
+    expect(segments, [
+      TextSegment('text with a '),
+      TextSegment('#hashtag', 'hashtag', true, false, false),
+      TextSegment(' and a '),
+      TextSegment('@mention', 'mention', false, true, false),
+      TextSegment('. '),
+      TextSegment('https://flutter.dev', 'https://flutter.dev', false, false, true),
+      TextSegment(' '),
+      TextSegment('#works', 'works', true, false, false),
+    ]);
+  });
+
+  test('Ignore mentions and hashtags that are not a word', () {
+    final segments = parseText('men@tion hash#tag');
+
+    expect(segments, [
+      TextSegment('men@tion hash#tag'),
+    ]);
+  });
+}
