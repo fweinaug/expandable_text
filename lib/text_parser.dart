3b6976443c35e7022a9f47474f7451f79e6c64bb
Stefan Sturm <stefan.sturm@me.com>
2021-05-28 16:04:15 +0200

    fix url parser

--- lib/text_parser.dart
+++ lib/text_parser.dart
@@ -8,11 +8,7 @@ class TextSegment {
 
   bool get isText => !isHashtag && !isMention && !isUrl;
 
-  TextSegment(this.text,
-      [this.name,
-      this.isHashtag = false,
-      this.isMention = false,
-      this.isUrl = false]);
+  TextSegment(this.text, [this.name, this.isHashtag = false, this.isMention = false, this.isUrl = false]);
 
   @override
   bool operator ==(Object other) =>
@@ -26,12 +22,7 @@ class TextSegment {
           isUrl == other.isUrl;
 
   @override
-  int get hashCode =>
-      text.hashCode ^
-      name.hashCode ^
-      isHashtag.hashCode ^
-      isMention.hashCode ^
-      isUrl.hashCode;
+  int get hashCode => text.hashCode ^ name.hashCode ^ isHashtag.hashCode ^ isMention.hashCode ^ isUrl.hashCode;
 }
 
 /// Split the string into multiple instances of [TextSegment] for mentions, hashtags, URLs and regular text.
@@ -47,7 +38,7 @@ List<TextSegment> parseText(String? text) {
 
   // parse urls and words starting with @ (mention) or # (hashtag)
   RegExp exp = RegExp(
-      r'(?<keyword>(#|@)([\p{Alphabetic}\p{Mark}\p{Decimal_Number}\p{Connector_Punctuation}\p{Join_Control}]+)|(?<url>(?:(?:https?|ftp):\/\/)?[\w/\-?=%.]+\.[\w/\-?=%.]+))',
+      r'(?<keyword>(#|@)([\p{Alphabetic}\p{Mark}\p{Decimal_Number}\p{Connector_Punctuation}\p{Join_Control}]+)|(?<url>(?:(?:https?|ftp):\/\/)?[-a-z0-9@:%._\+~#=]{1,256}\.[a-z0-9]{1,6}(\/[-a-zA-Z0-9():%_\+.~#?&\/=]*)?))',
       unicode: true);
   final matches = exp.allMatches(text);
 
@@ -69,8 +60,7 @@ List<TextSegment> parseText(String? text) {
     if (url != null) {
       segments.add(TextSegment(url, url, false, false, true));
     } else if (keyword != null) {
-      final isWord =
-          match.start == 0 || text.substring(match.start - 1, start) == ' ';
+      final isWord = match.start == 0 || text.substring(match.start - 1, start) == ' ';
       if (!isWord) {
         return;
       }
@@ -78,8 +68,7 @@ List<TextSegment> parseText(String? text) {
       final isHashtag = keyword.startsWith('#');
       final isMention = keyword.startsWith('@');
 
-      segments.add(
-          TextSegment(keyword, keyword.substring(1), isHashtag, isMention));
+      segments.add(TextSegment(keyword, keyword.substring(1), isHashtag, isMention));
     }
 
     start = match.end;
